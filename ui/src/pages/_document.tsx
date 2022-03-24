e07fa81dcf6fbd480e163bf5ed514b8dd1493972
y-temp4 <y.temp4@gmail.com>
2022-03-24 13:06:04 +0900

    Add Tailwind CSS and daisyUI

---
+++ ui/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html data-theme="light">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
