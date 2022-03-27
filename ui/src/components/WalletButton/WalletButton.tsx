50ce1e183eab7aeabd70820ecfe2160fc9df3a44
yuta17 <usgitan@gmail.com>
2022-03-27 10:58:51 +0900

    Add WalletButton

---
+++ ui/src/components/WalletButton/WalletButton.tsx
@@ -0,0 +1,51 @@
+import type { VFC } from 'react'
+
+import { useWalletMutators,useWalletState } from '~/state/wallet'
+
+const WalletButton: VFC = () => {
+  const Wallet = useWalletState()
+  const { setWallet } = useWalletMutators()
+
+  const connect = () => {
+    const ethereum = (window as any).ethereum
+    if (ethereum) {
+      ethereum.request({method: 'eth_requestAccounts'})
+        .then((result: Array<string>) => {
+          setWallet(result[0])
+        })
+    } else {
+      // TODO: error
+    }
+  }
+
+  const disconnect = () => {
+    setWallet('')
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button className="btn btn-info text-white rounded-full" onClick={() => connect()}>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const disconnectWalletButton = () => {
+    return (
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-info text-white rounded-full">{Wallet}</label>
+        <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+          <li><a onClick={() => disconnect()}>Disconnect</a></li>
+        </ul>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {Wallet.length > 0 ? disconnectWalletButton() : connectWalletButton()}
+    </>
+  )
+}
+
+export default WalletButton
