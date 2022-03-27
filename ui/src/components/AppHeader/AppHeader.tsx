a6851429cafafd9fb36aaa8ac264243b9ca3651b
yuta17 <usgitan@gmail.com>
2022-03-27 10:58:36 +0900

    Add AppHeader

---
+++ ui/src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,16 @@
+import type { VFC } from 'react'
+
+import WalletButton from '~/components/WalletButton'
+
+const AppHeader: VFC = () => {
+  return (
+    <div className="relative bg-white border-b">
+      <div className="flex items-center container mx-auto py-6 px-8">
+        <div className="text-2xl font-bold">DEMO</div>
+        <span className="ml-auto"><WalletButton /></span>
+      </div>
+    </div>
+  )
+}
+
+export default AppHeader
