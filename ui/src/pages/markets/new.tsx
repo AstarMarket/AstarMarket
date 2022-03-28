6a6a34eda476c76dfdb432f1897a1057bd7ad23f
yuta17 <usgitan@gmail.com>
2022-03-28 14:05:56 +0900

    Add markets/new page

---
+++ ui/src/pages/markets/new.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import MarketForm from '~/components/MarketForm'
+
+const New: NextPage = () => {
+  return (
+    <MarketForm />
+  )
+}
+
+export default New
