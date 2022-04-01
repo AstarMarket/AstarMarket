f9ee176231ea23d828d02da506fffeed556ee920
yuta17 <usgitan@gmail.com>
2022-04-01 23:09:46 +0900

    Add Link to root

--- ui/src/components/AppHeader/AppHeader.tsx
+++ ui/src/components/AppHeader/AppHeader.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import type { VFC } from 'react'
 
 import WalletButton from '~/components/WalletButton'
@@ -6,7 +7,9 @@ const AppHeader: VFC = () => {
   return (
     <div className="relative bg-white border-b">
       <div className="flex items-center container mx-auto py-6 px-8">
-        <div className="text-2xl font-bold">DEMO</div>
+        <Link href="/">
+          <a className="text-2xl font-bold">DEMO</a>
+        </Link>
         <span className="ml-auto">
           <WalletButton />
         </span>
