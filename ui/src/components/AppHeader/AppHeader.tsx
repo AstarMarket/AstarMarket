cb98081e9ddc70aedae87aa069f1e98bae6bcd49
yuta17 <usgitan@gmail.com>
2022-04-02 09:02:42 +0900

    Fix style

--- ui/src/components/AppHeader/AppHeader.tsx
+++ ui/src/components/AppHeader/AppHeader.tsx
@@ -5,8 +5,8 @@ import WalletButton from '~/components/WalletButton'
 
 const AppHeader: VFC = () => {
   return (
-    <div className="relative bg-white border-b">
-      <div className="flex items-center container mx-auto py-6 px-8">
+    <div className="relative max-w-5xl mx-auto bg-white">
+      <div className="flex items-center container mx-auto py-6 px-2">
         <Link href="/">
           <a className="text-2xl font-bold">DEMO</a>
         </Link>
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { InferGetServerSidePropsType } from 'next'
 import Link from 'next/link'
 
+import AppHero from '~/components/AppHero'
 import { prisma } from '~/lib/prisma'
 
 export const getServerSideProps = async () => {
@@ -13,14 +14,15 @@ export default function Index({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-      <p className="font-bold">Popular Markets</p>
+      <AppHero />
+      <p className="font-bold mt-8">Popular Markets</p>
       <ul className="grid grid-cols-3 gap-10 mt-4">
         {markets.map((market) => {
           return (
-            <Link key={market.id} passHref={true} href={`markets/${market.contract}`}>
-              <li className="p-5 border rounded cursor-pointer" key={market.id}>
+            <Link key={market.id} href={`markets/${market.contract}`}>
+              <a className="p-5 border rounded cursor-pointer" key={market.id}>
                 {market.title}
-              </li>
+              </a>
             </Link>
           )
         })}
