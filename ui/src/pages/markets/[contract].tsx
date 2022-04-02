5646f5ef5d37bb2ee3b315095e795c7a1660ddcd
yuta17 <usgitan@gmail.com>
2022-04-02 09:02:20 +0900

    Add show market page

---
+++ ui/src/pages/markets/[contract].tsx
@@ -0,0 +1,21 @@
+import type { GetServerSideProps,InferGetServerSidePropsType } from 'next'
+
+import { prisma } from '~/lib/prisma'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { contract } = context.query
+  const market = await prisma.market.findFirst({
+    where: { contract: contract as string },
+  })
+  return { props: { market } }
+}
+
+export default function Index({
+  market,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="mt-8">
+      <div className="p-5 border rounded">{market.title}</div>
+    </div>
+  )
+}
